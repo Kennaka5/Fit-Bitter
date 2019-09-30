854aaf1f80327ccd3250ed125e49b6ee3c3c6e7e
Ken Nakayama <kennaka5@gmail.com>
2019-09-30 16:53:46 -0700

    App Working pushing before i leave and switch ips

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import '../_mockLocation';
+import React, {useEffect, useState, useContext} from 'react';
 import  {StyleSheet} from 'react-native';
 import { Text } from 'react-native-elements';
-import {SafeAreaView} from 'react-navigation'
-import Map from '../components/Map'
+import {SafeAreaView} from 'react-navigation';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
+import Map from '../components/Map';
+import {Context as locationContext} from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null);
+    const { addLocation } = useContext(LocationContext)
+    
+    const startWatching = async () => {
+        try{
+            await requestPermissionsAsync();
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10
+            }, (location) => {
+                console.log(location)
+            })
+        } catch(error) {
+            setErr(error)
+        }
+    }
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
     return (
         <SafeAreaView
             forceIncet={{ top: 'always'}}
         >   
             <Text h2>Create a Track</Text>
             <Map />
+            {err ? <Text>Please enable location services</Text>: null}
         </SafeAreaView>
 )};
 
