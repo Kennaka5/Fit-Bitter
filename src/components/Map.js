7970fbbbe7353539f2e9447ab858c972136bd1fe
Ken Nakayama <kennaka5@gmail.com>
2019-10-16 16:40:04 -0700

    bug fixed! working polyline

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,8 +5,8 @@ import { Context as LocationContext } from '../context/LocationContext';
 
 
 const Map = () => {
-    const { state: {currentLocation} } = useContext(LocationContext);
-    console.log(currentLocation)
+    const { state: {currentLocation, locations} } = useContext(LocationContext);
+    // console.log(currentLocation)
     if (!currentLocation) {
         return <ActivityIndicator size="large" style={{marginTop: 200}}/>
     }
@@ -26,7 +26,9 @@ const Map = () => {
             strokeColor="rgba(158, 158, 255, 1.0)"
             fillColor="rgba(158, 158, 255, 0.3)"
         />
-        
+        <Polyline 
+            coordinates={locations.map(loc => loc.coords)}
+        />
         </MapView> 
     );
 }
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -9,15 +9,13 @@ import useLocation from '../hooks/useLocation';
 import TrackForm from '../components/trackForm';
 
 const TrackCreateScreen = ({isFocused}) => {
-    const { state, addLocation } = useContext(LocationContext)
+    const { state: {recording}, addLocation } = useContext(LocationContext)
 
-    const callback = useCallback(location => {
-        addLocation(location, state.recording)
-    }, [state.recording]);
+    const callback = useCallback( location => {
+        addLocation(location, recording)
+    }, [recording])
 
-    const [err] = useLocation(isFocused , (location) => {
-        addLocation(location, state.recording)
-    });
+    const [err] = useLocation(isFocused || recording, callback);
 
 
     return (
